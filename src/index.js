deb777b10efd537f46dc8b37e161dcad1fff4339
reardek <rebciopl@gmail.com>
2019-12-30 17:28:37 +0100

    first React Project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import List from './List'
+
+const App = () => {
+    return <List />
+};
+
+ReactDom.render(<App />, document.getElementById('root'))
